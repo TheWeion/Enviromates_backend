ed4178356088b6b671e36de57629849be92700b4
tom harcker <thackerstudent@gmail.com>
2022-08-08 09:46:53 +0100

    working tests

--- enviromates/models/events.py
+++ enviromates/models/events.py
@@ -6,12 +6,12 @@ class Events(db.Model):
     latitude = db.Column(db.Float, nullable=False)
     longitude = db.Column(db.Float, nullable=False)
     img_before = db.Column(db.String, nullable=False)
-    img_after = db.Column(db.String, nullable=False)
+    img_after = db.Column(db.String)
     title = db.Column(db.String, nullable=False)
     description = db.Column(db.String, nullable=False)
     difficulty = db.Column(db.Integer, nullable=False, default=1)
     start_date = db.Column(db.Date, nullable=False, default=datetime.now())
-    end_date = db.Column(db.Date, nullable=False)
+    end_date = db.Column(db.Date, )
 
     def __init__(self):
         return f"{self.id}"
--- init_db.py
+++ init_db.py
@@ -1,10 +1,10 @@
 from enviromates import db
-from enviromates.models.url import ShortUrls
 
 # Clear it all out
 
 db.drop_all()
-
+print("drop db")
 # Set it back up
 
 db.create_all()
+print("reinitialise db")
